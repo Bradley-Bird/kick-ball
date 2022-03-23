28fcefde9e7b403ece891bf6ecb778ffec03f2cc
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-23 14:11:07 -0700

    basic routes set up in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
-
+import Home from './views/Home';
+import Players from './views/Players';
+import Teams from './views/Teams';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/players">
+            <Players />
+          </Route>
+          <Route path="/teams">
+            <Teams />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
