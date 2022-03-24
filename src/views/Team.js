f3109a8c79a32a5fae36ba608ebb35c261405c71
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-24 11:16:49 -0700

    try catch  added to team.js

--- src/views/Team.js
+++ src/views/Team.js
@@ -6,13 +6,18 @@ import { fetchTeamId } from '../services/teams';
 function Team() {
   const params = useParams();
   const [teamInfo, setTeamInfo] = useState({ players: [] });
+  const [error, setError] = useState('');
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchTeamId(params.id);
-      setTeamInfo(data);
+      try {
+        const data = await fetchTeamId(params.id);
+        setTeamInfo(data);
+      } catch (e) {
+        setError(e.message);
+      }
     };
     fetchData();
   }, [params]);
-  return <TeamCard {...{ teamInfo }} />;
+  return error ? <p>{error}</p> : <TeamCard {...{ teamInfo }} />;
 }
 export default Team;
